40d24dad9684589f313560b6e91fa645b0cca1eb
hugo barthelemy <barthelemyhugo@gmail.com>
2017-09-14 11:43:58 +0200

    form with callback is ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   include Clearance::User
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :zip, presence: true, length: { minimum: 5 }
+  validates :city, presence: true
+  validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }, uniqueness: true
 end
