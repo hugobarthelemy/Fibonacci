78e61d8921d9f5d66d3e0c69d2942dfa6f37ca90
hugo barthelemy <barthelemyhugo@gmail.com>
2017-09-14 18:00:22 +0200

    stop bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,28 +4,46 @@ class UsersController < Clearance::UsersController
   end
 
   def create
-    @user = User.new(user_from_params)
+    @user = User.new(user_params)
 
-    if @user.save
-      sign_in @user
-
-      redirect_back_or url_after_create
+    if @user.first_name && @user.save
+      redirect_to edit_user_path(@user)
     else
+      @user.previous_step
       render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.change_ad_next_state && @user.update_attributes(advertisement_params)
+      redirect_to some_path_here && return
+    else
+      @user.previous_step
+      render :edit && return
+    end
+  end
+
+  def previous_step
+    @user = User.find(params[:id])
+    if !@user.loaded? && @user.previous_step && @user.save
+      redirect_to edit_user_path(@user)
+    else
+      redirect_to :back
     end
   end
 
   private
 
-  def user_from_params
-    params.require(:user).permit(:email,
-      :password,
-      :first_name,
-      :last_name,
-      :street_number,
-      :zip,
-      :city,
-      :situation,
-      :technical_informations)
-  end
+    def user_params
+      params.require(:user).permit(:email,
+        :password,
+        :first_name,
+        :last_name,
+        :street_number,
+        :zip,
+        :city,
+        :situation,
+        :technical_informations)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   include Clearance::User
+  include AASM
 
   validates :last_name, presence: true
   validates :first_name, presence: true
@@ -7,4 +8,35 @@ class User < ApplicationRecord
   validates :zip, presence: true, length: { minimum: 5 }
   validates :city, presence: true
   validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }, uniqueness: true
+
+  aasm do
+    state :loaded, :initial => true
+    state :profile_details
+    state :technical_details
+
+    event :fill_basic_info do
+      transitions :from => :loaded, :to => :profile_details, :guard => Proc.new { |o|
+      o.valid?
+    }
+    end
+
+    event :fill_skills do
+      transitions :from => :profile_details, :to => :technical_details
+    end
+
+
+    event :previous_step do
+      transitions :from => :technical_details, :to => :profile_details
+      transitions :from => :profile_details, :to => :loaded
+    end
+  end
+
+  def change_ad_next_state
+    case self.aasm_state
+    when 'loaded'
+      fill_basic_info
+    when 'profile_details'
+      fill_skills
+    end
+  end
 end
